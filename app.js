915550de89f08f5e14ade8ff6f2b2df6180ebe31
Arunprabu V <rgarun90@gmail.com>
2018-09-04 09:19:33 +0530

    Update app.js
    comments added

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+// Added for testing git commands
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
